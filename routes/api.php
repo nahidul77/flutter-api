df68ae0fc61a26894ceb54c8105de05b333331e9
Nahidul Islam <contact.nahidul@gmail.com>
2024-02-22 23:12:36 +0600

    Categories show

--- routes/api.php
+++ routes/api.php
@@ -20,4 +20,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 
-Route::get('categories', [CategoryController::class, 'index']);
+Route::apiResource('categories', CategoryController::class);
